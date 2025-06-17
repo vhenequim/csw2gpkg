1ba53ea141ff454c678b456412baf29cdf45e0f1
mauriciodev <mauricio.dev@gmail.com>
2025-06-17 11:56:59 -0300

    First commit

---
+++ csw2gpkg/__init__.py
@@ -0,0 +1,13 @@
+import owslib
+import geopandas as gpd
+
+def csw2gpkg(url, filename):
+    return None
+    """url = "http://bdgex.eb.mil.br/csw"
+    from owslib.csw import CatalogueServiceWeb
+    csw = CatalogueServiceWeb(url)
+    csw.getrecords2()
+    print(csw.results)
+    for rec in csw.records:
+        print(csw.records[rec].title)
+        print(csw.records[rec].bbox)"""
